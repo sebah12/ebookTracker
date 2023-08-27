36dec7fc323ef9a45bf4230cf5ed9a8f6513e8cc
sebah12 <sebah_12@hotmail.com>
2023-08-27 20:30:02 -0300

    Managers: Add method to get lowest historic price for an item

--- managers/price.py
+++ managers/price.py
@@ -26,6 +26,19 @@ class PriceManager:
         q = q.where(price.c.item_id == item_id).order_by(price.c.date.desc())
         return await database.fetch_one(q)
 
+    # TODO: get_lowest_item_price
+    @staticmethod
+    async def get_lowest_item_price(item_id):
+        """Get the lowest historic price (most recent) for the given item."""
+        q = price.select()
+        q = q.where(
+            price.c.item_id == item_id
+        ).order_by(
+                price.c.price, price.c.date.desc()
+        )
+        return await database.fetch_one(q)
+        return await database.fetch_one(q)
+
     @staticmethod
     async def insert_item_price(price_data):
         """Insert a new price in the prices table and return the row."""
@@ -34,5 +47,4 @@ class PriceManager:
         q = q.where(price.c.id == id_)
         return await database.fetch_one(q)
 
-    # TODO: get_lowest_item_price
     # TODO: delete_price
